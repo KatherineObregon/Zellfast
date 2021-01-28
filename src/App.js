bf3b2d2ad09d9372b44e75ef5c73b77447212953
Kevinjpg <a20182704@pucp.edu.pe>
2021-01-27 21:17:18 -0500

    header

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from "./components/Header";
+import "./components/css/App.css"
+
+function App(){
+    return <div className="app">
+    <Header />
+
+    </div>
+    
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCr18cQnhC0yqxd0d-Twb_ZOs7GVfx5hKc",
+    authDomain: "zellfast.firebaseapp.com",
+    projectId: "zellfast",
+    storageBucket: "zellfast.appspot.com",
+    messagingSenderId: "514524300217",
+    appId: "1:514524300217:web:40f9069b07c408b7d4ac68",
+    measurementId: "G-X3XB86WRZ8"
+  };
