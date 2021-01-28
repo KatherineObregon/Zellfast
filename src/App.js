ccd3940dbec42cd9846daa089c9922bfd55eda4e
Kevinjpg <a20182704@pucp.edu.pe>
2021-01-28 00:07:34 -0500

    Home 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from "./components/Header";
-import "./components/css/App.css"
+import "./components/css/App.css";
+import Home from "./components/Home"
 
 function App(){
     return <div className="app">
     <Header />
+    <Home />
 
     </div>
     
