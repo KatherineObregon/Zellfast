9ef1a77538bac34d0d2107a3df96e971eaeaed82
Katherine Obregon <a20181823@pucp.edu.pe>
2021-01-29 14:45:41 -0500

    Front end

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import { Link } from "react-router-dom"
 import Login from "./Login.js"
+import LandingPage from "./components/layout/LandingPage";
 
 function App(){
     return <div className="app">
-    <Header />
+    <LandingPage/>
+    {/* <Header />
     <Home />
       
     <Router>
         <Route path="/login">
           <Login></Login>
         </Route>
-      </Router>
+      </Router> */}
 
     </div>
     
---
+++ src/components/layout/LandingPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavbarContainer from "./Navbar/NavbarContainer"
+import ImageSlider from "./ImageSlider"
+import HomePageProductContainer from "./../HomePageProduct/HomePageProductContainer";
+import Footer from "./Footer";
+function LandingPage() {
+    return (
+        <React.Fragment>
+           <NavbarContainer />
+           <div className="container-fluid px-0">
+            <ImageSlider />
+            <HomePageProductContainer />
+            <Footer />
+           </div> 
+        </React.Fragment>
+    )
+}
+export default LandingPage;
