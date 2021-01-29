efca57d17e621d61ce0bb9bac596216b318b09c8
THEFLILUX <jorge.nicho@utec.edu.pe>
2021-01-28 22:31:50 -0500

    fix bug

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import React from 'react';
 import Header from "./components/Header";
 import "./components/css/App.css";
 import Home from "./components/Home"
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import { Link } from "react-router-dom"
 import Login from "./Login.js"
 
 function App(){
-    return <div className="app">
-    <Header />
-    <Home />
-      
+    return( <div className="app">
     <Router>
+      <Switch>
         <Route path="/login">
           <Login></Login>
         </Route>
-      </Router>
+        <Route path="/">
+          <Header />
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
 
     </div>
-    
+    )
 }
 
 export default App;
